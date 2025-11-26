4cbedf406fbdc695c0acfe41255c464e92d72886
egarrisxn <egarrisxn@gmail.com>
2025-11-25 20:47:17 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import { ThemeProvider } from "@/context/theme-provider";
+import { ThemeProvider } from "@/providers/theme";
 import "./globals.css";
 
 const geistSans = Geist({
